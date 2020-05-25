5af1747f3a675933d654cff04252ac8c663c573a
Tejashri3 <55490938+Tejashri3@users.noreply.github.com>
2020-05-25 14:42:50 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "stdlib.h"
+#include "Board.h"
+#include "Player.h"
+#include "GameLogic.h"
+
+#include <iostream>
+#include <vector>
+
+/*/
+AI
+Search for empty blocks with block filled under
+Make data structure with these blocks
+randomly choose a block from that data structure to spawn on
+
+*/
+
+int main()
+{
+    Board board;
+    Player player1, player2;
+    GameLogic gameLogic;
+
+    gameLogic.game(board, player1, player2, gameLogic);
+
+    char c;
+    std::cin >> c;
+    return 0;
+}
